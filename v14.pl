46379a89b4da5de4c930930cf1b5bc3016385f85
childgo <65421844+childgo@users.noreply.github.com>
2020-05-15 18:07:30 -0400

    Update and rename v13.pl to v14.pl

--- v14.pl
+++ v14.pl
@@ -4,6 +4,7 @@ $ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin';
 
 use warnings;
 use strict;
+print("List of All Domains and Username:")
 system ("cat /etc/trueuserdomains");
 
 
