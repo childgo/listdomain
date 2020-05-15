0fce57b2670df995c5f26f0ea70c5f4f978cb47c
childgo <65421844+childgo@users.noreply.github.com>
2020-05-15 17:21:57 -0400

    Update and rename v1.pl to v2.pl

--- v2.pl
+++ v2.pl
@@ -1,6 +1,25 @@
 #!/usr/bin/perl
 $ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin';
 
+sub print_info {
+    my $text = shift;
+    print BOLD YELLOW ON_BLACK $text;
+    print color 'reset';
+}
+
+sub print_warning {
+    my $text = shift;
+    print BOLD RED ON_BLACK "$text";
+    print color 'reset';
+}
+
+sub print_normal {
+    my $text = shift;
+    print BOLD CYAN ON_BLACK "$text";
+    print color 'reset';
+}
+
+
 
  open( USERDOMAINS, "/etc/trueuserdomains" );
     while (<USERDOMAINS>) {
