3605c6c924ad1abfde13a63789fba6210a6fb5e0
childgo <65421844+childgo@users.noreply.github.com>
2020-05-15 18:10:03 -0400

    Update and rename v17.pl to v18.pl

---
+++ v18.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+$ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin';
+
+
+use warnings;
+use strict;
+print("clear");
+print("#########################################:r \n");
+print("#####List of All Domains and Username:r \n");
+print("#########################################:r \n");
+system ("cat /etc/trueuserdomains");
+
+
