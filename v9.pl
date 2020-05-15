f35962c9db48b736f893fcf030fb7d20f91d29cf
childgo <65421844+childgo@users.noreply.github.com>
2020-05-15 17:48:55 -0400

    Update and rename v8.pl to v9.pl

---
+++ v9.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+$ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin';
+
+
+use warnings;
+use strict;
+
+
+system("bash cut -f "1" -d : /etc/trueuserdomains | sort | more");
+
+}
